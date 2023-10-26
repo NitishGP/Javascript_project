2a9fe104b5e4269c532738ce822eb2dbe934f99a
NitishGP <99392223+NitishGP@users.noreply.github.com>
2023-10-26 12:10:40 +0530

    Add files via upload

---
+++ Javascript_projects/stopwatch/index.js
@@ -0,0 +1,33 @@
+const CLOCK= document.querySelector(".clock");
+let sec=0;
+let hr=0;
+let min= 0;
+let IntervalID,clickcount;
+
+const StartWatch=()=>{
+clickcount++;
+if (clickcount == 1) {
+    IntervalID=setInterval(()=>{
+    if(sec<59)
+    sec++;
+    
+    else if(min>59){
+        min=0;
+        hr++;
+    }
+    else {
+        sec=0;
+        min++;
+    }
+    console.log(`${String(hr).padStart(2,"0")}:${String(min).padStart(2,"0")}:${String(sec).padStart(2,"0")}`);
+    CLOCK.innerText=`${String(hr).padStart(2,"0")}:${String(min).padStart(2,"0")}:${String(sec).padStart(2,"0")}`;
+
+
+},1000);
+    }
+};
+
+const StopWatch=()=>{
+clearInterval(IntervalID);
+clickcount=0;
+};
